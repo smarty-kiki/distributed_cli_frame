25186ebb95d56a130fab2099294afc676e9dc6fc
yaoyang <kiki@smarty.so>
2018-03-08 15:35:25 +0800

    初始化提交

---
+++ project/tool/naming_project.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function sed_name
+{
+    cat $1 | sed -e "s/distributed_cli_frame/$2/g" > $1.new && mv $1.new $1
+}
+
+if [ ! -n "$1" ] ;then
+    echo "Usage: $0 <name>"
+    exit
+fi
+
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../../
+
+sed_name $ROOT_DIR/project/config/development/supervisor/queue_worker.conf $1
+sed_name $ROOT_DIR/project/config/production/supervisor/queue_worker.conf $1
+sed_name $ROOT_DIR/project/tool/start_dev_server.sh $1
---
+++ project/tool/start_dev_server.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../..
+
+DEP_FILE=$ROOT_DIR/dep_service_list
+if [ ! -f $DEP_FILE ]
+then
+    echo "DEP_FILE 不存在，检查 $DEP_FILE"
+    exit
+fi
+
+sh $ROOT_DIR/project/tool/dep_build.sh link
+
+DEP_SERVICE_VOLUMN="-v $ROOT_DIR/../frame:/var/www/frame"
+LINK=""
+while read line
+do
+    SERVICE_NAME=`echo $line | grep -v ^# | awk '{print $1}'`
+    if [ "$SERVICE_NAME" != "" ]
+    then
+        DEP_SERVICE_VOLUMN="$DEP_SERVICE_VOLUMN -v $ROOT_DIR/../$SERVICE_NAME:/var/www/$SERVICE_NAME"
+
+        if docker exec $SERVICE_NAME echo ok > /dev/null 2>&1
+        then
+            LINK="$LINK --link $SERVICE_NAME:$SERVICE_NAME";
+        fi
+    fi
+done<$DEP_FILE
+
+sudo docker run --rm -ti -p 80:80 -p 3306:3306 --name distributed_cli_frame \
+    $LINK $DEP_SERVICE_VOLUMN \
+    -v $ROOT_DIR/:/var/www/distributed_cli_frame \
+kikiyao/debian_php_dev_env start
